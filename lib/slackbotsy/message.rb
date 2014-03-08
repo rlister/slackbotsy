f556d9bec6a44ba849b0cd4e25f288f33f44bee9
Richard Lister <rlister@gmail.com>
2014-03-08 12:54:29 -0500

    add convenience getters for message properties

--- lib/slackbotsy/message.rb
+++ lib/slackbotsy/message.rb
@@ -16,6 +16,13 @@ module Slackbotsy
       @caller.say(text, options)
     end
 
+    ## convenience getter methods for message properties
+    %w[ token team_id channel_id channel_name timestamp user_id user_name text ].each do |method|
+      define_method(method) do
+        self.fetch(method, nil)
+      end
+    end
+    
   end
 
 end
